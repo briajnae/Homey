81c91f34c763f09d7dcc7151586acbff0225ea0a
Jenna's Bootcamp <work@Jennas-MacBook-Pro.local>
2018-03-07 16:09:08 -0600

    adds zillow and google ajax

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,1 +1,16 @@
-//ayyeee homeys checking github connection
+// *********************************************************************************
+// api-routes.js - this file offers a set of routes for displaying and saving data to the db
+// *********************************************************************************
+
+// Dependencies
+var js = require("../data/homey.js");
+var nodeZillow = require("../routing/javascript.js");
+
+// Routes
+module.exports = function(app) {
+
+    app.get("/api/all", function(req, res) {
+  
+    });
+};
+
