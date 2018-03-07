3f6b1842771af2da1d38021e21c31230df5e42b2
Samidh Desai <samidhdesai@gmail.com>
2018-03-07 11:56:39 -0600

    fixes css in server and also adds one html route

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -1,12 +1,12 @@
 var path = require("path");
 
 module.exports = function(app) {
-    // HTML GET Requests
-    // Below code handles when users "visit" a page.
-    // In each of the below cases the user is shown an HTML page of content
-    // ---------------------------------------------------------------------------
-  
+    //index route loads the landing page
     app.get("/", function(req, res) {
       res.sendFile(path.join(__dirname, "../public/index.html"));
     });
+    //home loads the home page where results will be searched. 
+    ;app.get("/home", function(req, res) {
+        res.sendFile(path.join(__dirname, "../public/home.html"));
+    })
 }
