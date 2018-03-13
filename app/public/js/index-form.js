3615e391a267918f625e14aaaee62a06ae38fcf6
Samidh Desai <samidhdesai@gmail.com>
2018-03-13 18:16:30 -0500

    appends results onto the page

--- app/public/js/index-form.js
+++ app/public/js/index-form.js
@@ -2,6 +2,8 @@ $(document).ready(function(){
   
   $("#submit-index").on("click", function(e){
     e.preventDefault();
+    
+    // window.location.href ='/home.html';
     var data_object = { 
       zip_code: $("#zipcode").val().trim(),
       property_type: $("#propertyType").val(),
@@ -17,15 +19,31 @@ $(document).ready(function(){
         retry(data_object);
       }
       else{
+        for (i = 0; i < data.length; i++) { 
+          $("#results").append("<li>" + data[i].community_area_name + ", Property type: " + data[i].property_type + ", Property Name:  " + data[i].property_name + ", Address: " + data[i].address + "</li>")
+        }
+         
         
+        
+        // $(".listingCards").text("<p>" + data[0].community_area_name + "</p>");
+        // $(".listingCards").text("<p>" + data[0].property_type + "</p>");
+        // $(".listingCards").text("<p>" + data[0].zip_code + "</p>");        
+        // console.log("data community " + data[0].community_area_name);
+        // console.log("data prop " + data[0].property_type);
+        // console.log("data zip " + data[0].zip_code);
       }
     })
     const retry = function(data_object){
       $.post("/api/retry/", data_object, function(){
       }).then(function(data){
         console.log("retried data", data)
+        for (i = 0; i < data.length; i++) { 
+          $("#results").append("<li>" + data[i].community_area_name + ", Property type: " + data[i].property_type + ", Property Name:  " + data[i].property_name + ", Address: " + data[i].address + "</li>")
+        }
+
       });
-    };                      
+    }; 
+                   
   });
 
      
