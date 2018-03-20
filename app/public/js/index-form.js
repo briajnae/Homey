3985931ccfa0001c9ebc15284ade5fab575cd463
Jenna's Bootcamp <work@dhcp-10-102-249-53.wireless.northwestern.private>
2018-03-20 14:47:32 -0500

    adds json commenting

--- app/public/js/index-form.js
+++ app/public/js/index-form.js
@@ -1,30 +1,40 @@
 $(document).ready(function(){
-  
+
+  /* on Click for submit */
   $("#submit-index").on("click", function(e){
     e.preventDefault();
+    /* takes in parameters from drop down menu */
     var data_object = { 
       property_type: $("#propertyType").val(),
       community_area_name: $("#neighborhood").val()
     }
+    
     console.log("hello", data_object);
+    /* takes input from user and sets local storage */
     localStorage.setItem("userInput", JSON.stringify(data_object));
-    //changes from index to 
-    window.location.href="/home.html";
-    localStorage.getItem("userInput");
+    /* localStorage.getItem("userInput");*/
+
+    
+    /* post request that matches user input with data from server/database */
     $.post("/api/Affordable_housing", data_object, function(){
       console.log("inside the post", data_object)
     }).then(function(data){
       console.log("hehehehe", data);
+  
+    /* if data isn't retrieve then retry function */
       if(!data.length){
         console.log("send to retry")
         retry(data_object);
       }
+    /* if data then append */
       else{
         for (i = 0; i < data.length; i++) { 
           $("#results").append("<li>" + data[i].community_area_name + ", Property type: " + data[i].property_type + ", Property Name:  " + data[i].property_name + ", Address: " + data[i].address + "</li>")
         }
       }
     })
+    //retry function if no resrults show up
+    /* if no immediate response then retry function to attempt to append that again */
     const retry = function(data_object){
       $.post("/api/retry/", data_object, function(){
       }).then(function(data){
@@ -32,20 +42,10 @@ $(document).ready(function(){
         for (i = 0; i < data.length; i++) { 
           $("#results").append("<li>" + data[i].community_area_name + ", Property type: " + data[i].property_type + ", Property Name:  " + data[i].property_name + ", Address: " + data[i].address + "</li>")
         }
-
       });
-
     }; 
                    
-
   });
-
      
 });
-
-
-            
-  
-
-
 
