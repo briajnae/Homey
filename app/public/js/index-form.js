bb3b9fe562aa910bdea00114c0b55ec6a8de399f
Samidh Desai <samidhdesai@gmail.com>
2018-03-20 23:04:51 -0500

    adds comments to the google maps function

--- app/public/js/index-form.js
+++ app/public/js/index-form.js
@@ -7,20 +7,16 @@ $(document).ready(function () {
       property_type: $("#propertyType").val(),
       community_area_name: $("#neighborhood").val()
     }
-    console.log("hello", data_object);
-  
     $.post("/api/Affordable_housing", data_object, function () {
       console.log("inside the post", data_object)
     }).then(function (data) {
-      console.log("hehehehe", data);
       if (!data.length) {
-        console.log("send to retry")
         retry(data_object);
       }
       else {
         for (i = 0; i < data.length; i++) {
           
-          $("#results").append("<div id=results-list class=col-md-4 col-lg-4>" + "<img src='" + queryURL + "'> " + "</img>" + "<h3>" + data[i].community_area_name + "</h3>" + "<h4>" + data[i].property_name + "</h4>" + "<p>" + data[i].address + "<p>" + "<p>" + data[i].property_type + "<p>" + "</div>");  
+          $("#results").append("<div id=results-list class=col-md-4 col-lg-4>" + "<img src='" + queryURL + "'> " + "</img>" + "<h3>" + data[i].community_area_name + "</h3>" + "<h4>" + data[i].property_name + "</h4>" + "<p>" + data[i].address + "<p>" + "<p>" + data[i].property_type + "<p>" + "<button id=map-button onclick=window.location.href='/home.html'>" + "View Map" + "</button>" + "</div>");  
           updateMap();
 
           var location = data[i].location;
@@ -28,11 +24,8 @@ $(document).ready(function () {
           var apiKey = "AIzaSyD0KYDrGLg-xZIwXfNvcnCj9_p4yn-3bz8";
           var queryURL = "https://maps.googleapis.com/maps/api/streetview?" + "size=200x150" + "&location" + location + apiKey;
           
-          
           localStorage.setItem("dataArr", JSON.stringify(data));
-          // window.location.href="/index.html";
-          console.log(queryURL);
-
+          
         }
       }
     });
@@ -40,31 +33,22 @@ $(document).ready(function () {
     const retry = function (data_object) {
       $.post("/api/retry/", data_object, function () {
       }).then(function (data) {
-        console.log("retried data", data)
-        // localStorage.setItem("dataArr", JSON.stringify(data));
-        // window.location.href="/index.html";
         for (i = 0; i < data.length; i++) {
           $("#results").append("<div id=results-list class=col-md-4 col-lg-4>" + "<img class=api-img src='" + queryURL + "'> " + "</img>" + "<h3>" + data[i].community_area_name + "</h3>" + "<h4>" + data[i].property_name + "</h4>" + "<p>" + data[i].address + "<p>" + "<p>" + data[i].property_type + "<p>"+ "<button id=map-button onclick=window.location.href='/home.html'>" + "View Map" + "</button>" + "</div>");
          
-
           var location = data[i].location;
           var use = location.substr(1).slice(0, -1);
           var yes = use.split(" ").join("");
-          console.log(yes);
-          
+        
           var apiKey = "AIzaSyD0KYDrGLg-xZIwXfNvcnCj9_p4yn-3bz8";
           var queryURL = "https://maps.googleapis.com/maps/api/streetview?" + "size=300x250" + "&location=" + yes + "&key=" + apiKey;
           
-       
           localStorage.setItem("dataArr", JSON.stringify(data));
-        // window.location.href="/index.html";
-          console.log(queryURL);
+
         }
       });
 
-
     };
-
 
   }); 
 
